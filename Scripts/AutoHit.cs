1a71eecd378d6acccc85f82648eb7e7ec27df2a8
Mawootad <adinfinatioma@gmail.com>
2025-07-19 12:52:32 -0700

    Add current buff rank getter

--- Scripts/AutoHit.cs
+++ Scripts/AutoHit.cs
@@ -28,6 +28,7 @@ using System.Linq;
 using Kingmaker.Utility;
 using Kingmaker.Controllers.TurnBased;
 using Kingmaker.PubSubSystem;
+using Kingmaker.EntitySystem.Properties.BaseGetter;
 
 class ClassesWithGuid
 {
@@ -38,6 +39,7 @@ class ClassesWithGuid
 			(typeof(ApplyBuffStackedByCaster), "7948892518980b855c341fdadac45263"),
 			(typeof(RemoveBuffStacksByCaster), "d08c6f8712f9ceb821185aa7f4cbf05c"),
 			(typeof(ToggleUiIfCurrentUiOwnerHasFeatureAndNotCaster), "a38f30053d7a26d8cf92f598d2346ebc"),
+			(typeof(CurrentRankGetter), "632487675d6d954fbec7898dbd252e97"),
 		};
 }
 
@@ -288,5 +290,18 @@ namespace OperativeRework
 		{
 			if (buff == Fact as Buff) UpdateBuffs();
 		}
+	}
+
+	[TypeId("632487675d6d954fbec7898dbd252e97")]
+	public class CurrentRankGetter : MechanicEntityPropertyGetter, PropertyContextAccessor.IOptionalFact
+	{
+
+		protected override int GetBaseValue()
+		{
+			Buff buff = this.GetFact() as Buff;
+			return buff?.Rank ?? 0;
+		}
+
+		protected override string GetInnerCaption(bool useLineBreaks) => "Rank of current buff";
 	}
 }
